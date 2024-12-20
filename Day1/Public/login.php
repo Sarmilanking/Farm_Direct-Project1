5540d7b05f9ce97f2083cfe9b1aff1da080fdfa2
Sai_Sarmi <139688376+Sarmilanking@users.noreply.github.com>
2024-12-20 10:22:23 +0000

    contact updated

--- Day1/Public/login.php
+++ Day1/Public/login.php
@@ -105,7 +105,7 @@
 	
 		<footer id="footer" class="container" style ="background: #fff; color: black; width: 100%; ">
 										<hr style = "border-top: 1px solid #ccc;"><br/><br/><br/>
-										<p align = "center">Contact Us: (234) 8133936723
+										<p align = "center">Contact Us: (+94) 0762552365
 											&copy; FarmConnect. All rights reserved</p>
 								
 		</footer>
